f8c0ec5b1a9feb0c1acd353700e307f03974075c
Melody22150 <melodyvoymant@yahoo.fr>
2025-07-23 15:22:59 +0200

    MAJ HTML + CSS

---
+++ assets/js/script_article.js
@@ -0,0 +1,13 @@
+// JS MENU BURGER
+
+  const burgerBtn = document.querySelector(".burger-button");
+  const burgerContainer = document.querySelector(".burger-container");
+
+  burgerBtn.addEventListener("click", () => {
+    burgerContainer.classList.toggle("active");
+  });
+    burgerBtn.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" || e.key === " ") {
+    burgerContainer.classList.toggle("active");
+  }
+});
--- assets/js/script_articles.js
+++ assets/js/script_articles.js
@@ -119,3 +119,15 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("Catégorie sélectionnée :", selectCategorie.value);
   });
 });
+
+  const burgerBtn = document.querySelector(".burger-button");
+  const burgerContainer = document.querySelector(".burger-container");
+
+  burgerBtn.addEventListener("click", () => {
+    burgerContainer.classList.toggle("active");
+  });
+    burgerBtn.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" || e.key === " ") {
+    burgerContainer.classList.toggle("active");
+  }
+});
